ac9be5451388a36d1c7f7c0cf9695b0a795247a5
Ziabeher Phillips <zabq02@gmail.com>
2022-05-18 19:53:35 -0400

    Results Main UI done

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -33,16 +33,18 @@ const Results = () => {
   return (
     <>
     <Header/>
-    <div className="mt-20 grid grid-cols-3 gap-y-10 items-center w-full">
+    <div className="pt-20 grid grid-cols-3 gap-y-10 items-center w-full bg-gray-300">
         {results.map((dish) => (
             <div className="flex flex-col bg-red-500 w-3/5 rounded-2xl justify-self-center" key={dish.idMeal}>
                 <div className="dish-img">
-                <img className="w-full object-cover" src={dish.strMealThumb} alt="" />
+                <img className="w-full object-cover rounded-t-lg" src={dish.strMealThumb} alt="" />
                 </div>
 
                 <div className="dish-info">
-                <h1 className='text-white text-center text-2xl pt-3 font-dancing-script'>{dish.strMeal}</h1>
-                <p className="px-6 py-20 text-white">Region: {dish.strArea}</p>
+                <h1 className='text-white text-center text-4xl pt-3 font-dancing-script'>{dish.strMeal}</h1>
+                <p className="px-6 mt-5 text-white text-lg">Region: {dish.strArea}</p>
+                <p className="px-6 text-white category text-lg">Category: {dish.strCategory}</p>
+                <p className="tags px-6 pb-5 text-white text-lg">Tags: {dish.strTags}</p>
                 </div>
             </div>
         ))}
