a9eccad1c96486b71b212b7075a75f36a44807a1
Ziabeher Phillips <zabq02@gmail.com>
2022-05-27 11:40:59 -0400

    favorites bug fixes

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -16,6 +16,7 @@ const Modal = ({selectedDish, state, closeModal}) => {
 
   useEffect(() => {
     if (favoritesContext === undefined) {
+      setFavoriteDishes(JSON.parse(localStorage.getItem('favoriteDishes')))
       return 
     } else {
       setFavoriteDishes(favoritesContext)
